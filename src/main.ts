9f09b96631972acb9d459d0bc37fd8b023044eb0
bruno-devbr <brunoqueiroz0707@gmail.com>
2026-01-02 16:01:30 -0300

    feat: add clouds on the game

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { k } from "./utils/global/kaplay";
+import { setCloud } from "./utils/sprites/clouds";
 import { setupFloor } from "./utils/sprites/floor";
 
 k.setGravity(1000);
 
 setupFloor(false);
+
+let minTime = 5;
+let maxTime = 9;
+
+const time = Math.floor(Math.random() * (maxTime - minTime + 1) + minTime);
+setCloud(true);
+
+setInterval(() => {
+    setCloud(false);
+}, time * 1000);
---
+++ src/utils/sprites/clouds.ts
@@ -0,0 +1,26 @@
+import { k } from "../global/kaplay";
+
+k.loadRoot("/assets/");
+k.loadSprite("cloud", "cloud.png");
+
+export function setCloud(value: boolean) {
+    const minHeight = 100;
+    const maxHeight = 300;
+
+    const height = Math.floor(
+        Math.random() * (maxHeight - minHeight + 1) + minHeight
+    );
+
+    const cloud = k.add([k.sprite("cloud"), k.pos(k.width(), height), "cloud"]);
+
+    if (!value) return;
+
+    cloud.onUpdate(() => {
+        cloud.move(-25, 0);
+
+        if (cloud.pos.x < 0) {
+            cloud.destroy();
+            setCloud(true);
+        }
+    });
+}
